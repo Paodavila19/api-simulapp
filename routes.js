98049560b048ffac523292f26b8ee6998cc5ebb6
Paodavila19 <pandrea.davila@udea.edu.co>
2023-04-13 15:42:55 -0500

    mi primer comentario

---
+++ routes.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM users', (err, rows)=>{
+            if(err) return res.send(err)
+            res.json(rows)
+        })
+    })
+})
+
+routes.post('/', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('INSERT INTO users set ?', [req.body], (err, rows)=>{
+            if(err) return res.send(err)
+            res.send('user add')
+        })
+    })
+})
+
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('DELETE FROM users WHERE id = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+            res.send('user delete')
+        })
+    })
+})
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('UPDATE users set ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+            res.send('user updated')
+        })
+    })
+})
+
+module.exports = routes
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+const routes = require('./routes')
+const cors = require("cors");
+
+
+const app = express()
+app.set('port', process.env.PORT || 9000)
+const dbOptions = {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: 'password',
+    database: 'test'
+}
+
+// middlewares ------------------
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json())
+app.use(cors());
+
+
+// routes -----------------------
+app.get('/',(req, res)=>{
+    res.send('Welcome to my API')
+})
+app.use('/users', routes)
+
+// server running ----------------
+app.listen(app.get('port'), () => {
+    console.log('server is running on port', app.get('port'))
+})
+
